281552ec10602f33d83522bdb7be414ec6e58806
ed519 <edgartorosyan1999@mail.ru>
2020-03-08 02:23:47 +0400

    Practical

---
+++ Week1/Practical/Problem2.py
@@ -0,0 +1,12 @@
+course = "Intro to Python"
+student = "Edgar Torosyan"
+nickname = "Ed"
+grade = 21
+pythonExpert = False
+
+print("course:",course)
+print("student:",student)
+print("nickname:",nickname)
+print("grade:",grade)
+print("python expert:",pythonExpert)
+
---
+++ Week1/Practical/Problem4.py
@@ -0,0 +1,6 @@
+ab = 3
+ac = 4 
+
+bc = (ab**2 + ac**2)**(1/2)
+print("The hypotenuse of the triangle ABC =", bc)
+
