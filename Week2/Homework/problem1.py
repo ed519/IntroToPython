0f8045e22231fee10f709b7651ab7c2895c50c84
ed519 <edgartorosyan1999@mail.ru>
2020-03-21 11:01:46 +0400

    W2 Homework

--- Week2/Homework/problem1.py
+++ Week2/Homework/problem1.py
@@ -1,11 +1,14 @@
 import datetime
 
 d_now = datetime.datetime.now()
+
+print("Corrent date:" , d_now)
 num_y = input()
-#num_d = input()
+num_d = input()
 
-if num_y is None:
-    print("Num_Y is empty")
-    
+print("Given years:", num_y)
+print("Given days:" , num_d)
 
+t_delta = datetime.timedelta(days =int(num_y) * 365 + int(num_d) )
 
+print("Final date:", d_now + t_delta)
